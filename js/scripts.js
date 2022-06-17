f82e2f70a6e1485c32d05ccf82ce8a6d06e803fd
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-06-17 16:46:06 -0700

    WIP, test in console and console log why price is blank

--- js/scripts.js
+++ js/scripts.js
@@ -18,21 +18,26 @@ function Sizes(large, medium) {
 
 let pizzaOne = new Pizza();
 
-Pizza.prototype.calculatePizzaPrice = function () {
-  //const basePrice = 10;
-  let output = 10;
+Sizes.prototype.calculatePizzaPrice = function () {
+  let basePrice = 10;
+  let output = [basePrice];
+  if (this.large) {
+    output[0] +5;
+    return output;
+  }
 
-  return output;
 };
+console.log(output)
 
 // UI Logic
 $(document).ready(function () {
   $("#buy-button").submit(function (event) {
     event.preventDefault();
-    const topping = $("#topping").val();
+    pizzaOne.calculatePizzaPrice();
+    //const topping = $("#topping").val();
     const size = $("#size").val();
-    let finalPizza = pizzaOne.calculatePizzaPrice();
-    $("#pizza-price-msg").text(finalPizza);
+    let finalPizza = calculatePizzaPrice(size);
+    $("#pizza-price-msg").text(pizzaOne.large);
     //return new Pizza(mushroom, garlic, artichoke, large, medium);
   });
 });

cf70f23f9ca559b6d07c54c77ea9aa9886e2ecc4
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-06-17 15:15:32 -0700

    Test 1 WIP.  Add method for Pizza price, continue to work on Test

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,38 @@ function Pizza(mushroom, garlic, artichoke, large, medium) {
   this.artichoke = artichoke;
   this.large = large;
   this.medium  = medium;
-  this.pizzaPrice = this.calculatePizzaPrice();
-}
+  
+};
+
+let pizzaOne = new Pizza();
+  
+
+Pizza.prototype.calculatePizzaPrice = function() {
+  //const basePrice = 10;
+  let output = 10;
+  
+  return output;
+
+  };
+  
+  
+
+
+
+
+
+// UI Logic
+$(document).ready(function() {
+  $("#buy-button").submit(function(event) {
+    event.preventDefault();
+    const topping = $("#topping").val();
+    const size = $("#size").val();
+    let finalPizza = pizzaOne.calculatePizzaPrice();
+    $("#pizza-price-msg").text(finalPizza);
+    //return new Pizza(mushroom, garlic, artichoke, large, medium);
+  });
+});
+
+
 
 
